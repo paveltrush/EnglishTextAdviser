42c4e8a5d1ccf5e288eff37f9cbc629afc35aaf6
pavel <paveltrush019@gmail.com>
2023-08-29 22:22:10 -0400

    docker improvements

--- docker/build-local.sh
+++ docker/build-local.sh
@@ -1,4 +1,3 @@
-
 echo Building LINUX-LOCAL docker environment
 docker-compose build --force-rm
 
@@ -6,8 +5,8 @@ echo Docker Container start
 docker-compose up -d
 
 echo Building Vendor for PHP
-docker-compose exec php bash -c "apt-get update && apt-get install git && git config core.fileMode false && cd /var/www/html composer install -v && exit"
-cp ..\.env.example ..\.env
+docker-compose exec php bash -c "apt-get update && cd /var/www/html && composer install"
+docker-compose exec php bash -c "git config --global --add safe.directory /var/www/html && git config core.fileMode false && exit"
+cp ../.env.example ../.env
 docker-compose exec php bash -c "php artisan key:generate"
-docker-compose exec php bash -c "ln -s /var/www/html/storage/app/public/uploads /var/www/html/public/uploads"
-
+docker-compose exec php bash -c "php artisan migrate && exit"
